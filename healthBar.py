cd2e90a7cf00657f7831396437abea09ac715bb2
Emir Kars <emirkars97@gmail.com>
2022-04-25 22:23:56 +0300

    Added + HealthBar class + all Enemy methods generalized for varying frame counts for different enemy types + idle methods + player control binded to "A" key + player and enemy attack functionality ToDo * wrap all code for moves in a single method * make sure that method is using the action cooldown * fix accuracy * start handling effects + add status icons + user prompt via panel + more moves + improve comments

---
+++ healthBar.py
@@ -0,0 +1,10 @@
+import pygame
+class HealthBar:
+    def __init__(self, x, y, hp, max_hp):
+        self.x=x
+        self.y=y
+        self.hp=hp
+        self.max_hp=max_hp
+    def draw(self, ratio, screen):
+        pygame.draw.rect(screen, (255,0,0), (self.x, self.y, 75, 10 ))
+        pygame.draw.rect(screen, (0, 255, 0), (self.x, self.y, 75*ratio, 10))
